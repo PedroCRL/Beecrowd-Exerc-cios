ee4f8fa2444d634b4f80fb67a7669cf46bc5ec66
Pedro C <pedrocrl@terra.com.br>
2025-10-22 16:26:20 -0300

    atualizando exercicios

---
+++ Beginner/1008.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = float(input())
+total = b * c
+print(f"NUMBER = {a}")
+print(f"SALARY = U$ {total:.2f}")
---
+++ Beginner/1010.py
@@ -0,0 +1,4 @@
+c1,n1,p1 = list(map(float, input().split()))
+c2,n2,p2 = list(map(float, input().split()))
+total = (n1*p1)+(n2*p2)
+print(f"VALOR A PAGAR: R$ {total:.2f}")
