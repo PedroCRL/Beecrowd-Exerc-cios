b8acfd0c96e42b3c40a78b39604ac9e5dc1b588c
Pedro C <pedrocrl@terra.com.br>
2025-10-27 17:15:32 -0300

    Atualização de exercicios

---
+++ Beginner/1011.py
@@ -0,0 +1,4 @@
+pi=3.14159
+r=float(input())
+v=(4/3.0)*pi*r**3
+print("VOLUME = %.3f" %v)
---
+++ Beginner/1012.py
@@ -0,0 +1,12 @@
+A,B,C=list(map(float,input().split()))
+pi=3.14159
+t=(A*C)/2
+circulo=pi*C**2
+trapezio=((A+B)*C)/2
+quadrado = (B*B)
+retangulo = (A*B)
+print("TRIANGULO: %.3f" %t)
+print("CIRCULO: %.3f" %circulo)
+print("TRAPEZIO: %.3f"%trapezio)
+print("QUADRADO: %.3f"%quadrado)
+print("RETANGULO: %.3f"%retangulo)
